5b143ffce03d9687170368b753f830d477be9d7b
zyjnicemoe <zhuyijun1317453947@outlook.com>
2020-07-15 10:56:04 +0800

    first commit

---
+++ src/page/about/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+require('page/common/header/index.js');
+require('page/common/nav/index.js');
+var navSide = require('page/common/nav-side/index.js');
+navSide.init({name:'about'});
