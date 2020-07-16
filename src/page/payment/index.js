f130f680534682433712fc9482b3625badff3665
zyjnicemoe <zhuyijun1317453947@outlook.com>
2020-07-16 12:10:28 +0800

    update page

---
+++ src/page/payment/index.js
@@ -0,0 +1,48 @@
+require('./index.css')
+require('page/common/nav/index.js');
+require('page/common/header/index.js');
+var _mm=require('util/mm.js');
+var templateIndex = require('./index.string');
+var _payment = require('service/payment-service.js');
+//page逻辑部分
+var page={
+    data : {
+        orderNumber : _mm.getUrlParam('orderNumber')
+    },
+    init:function () {
+        this.onLoad();
+    },
+    onLoad : function () {
+        //加载detail数据
+        this.loadPaymentInfo();
+    },
+    //加载订单列表
+    loadPaymentInfo : function () {
+        var _this = this,
+            paymentHtml = '',
+            $pageWarp = $('.page-warp');
+        $pageWarp.html('<div class="loading"></div>')
+        _payment.getPaymentInfo(this.data.orderNumber,function (res) {
+            //渲染html
+            paymentHtml = _mm.renderHtml(templateIndex,res);
+            $pageWarp.html(paymentHtml);
+            _this.listenOrderStatus();
+        },function (errMsg) {
+            $pageWarp.html('<p class="err-tip">' + errMsg + '</p>')
+        });
+    },
+    //监听订单状态
+    listenOrderStatus : function () {
+        var _this = this;
+        this.paymentTimer = window.setInterval(function () {
+            _payment.getPaymentStatus(_this.data.orderNumber,function (res) {
+                if(res == true){
+                    window.location.href = './result.html?type=payment&orderNumber'+_this.data.orderNumber;
+                }
+            })
+        },5e3)
+    }
+};
+$(function () {
+    page.init();
+})
